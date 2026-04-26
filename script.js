5229e0d63aaa9393af8afba531b19a795c2fc321
UnknownOP-cyber <hassan512prince@gmail.com>
2026-04-26 16:15:45 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+ AOS.init({ duration: 800, once: true });
+        window.onload = function () {
+            Swal.fire({
+                title: 'NATURE HAS EVOLVED',
+                text: 'Welcome to the v4.2 Intel Portal.',
+                background: '#0b0b0b',
+                color: '#ff9c00',
+                confirmButtonColor: '#2ecc71',
+                confirmButtonText: 'ENTER ERANGEL'
+            });
+        };
